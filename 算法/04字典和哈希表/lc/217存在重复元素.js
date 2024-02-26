595c876a67dd3c1ba6dfe735b8235af55d7e8f75
apollo <1550588466@qq.com>
2024-02-26 21:16:59 +0800

    feat: 217 存在重复元素 new Set

--- 算法/04字典和哈希表/lc/217存在重复元素.js
+++ 算法/04字典和哈希表/lc/217存在重复元素.js
@@ -3,18 +3,26 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-  const map = new Map()
+  // const map = new Map()
   // for (let i = 0; i < nums.length; i++) {
   //   if (map.has(nums[i])) {
   //     return true
   //   }
   //   map.set(nums[i], i)
   // }
+  // for (const x of nums) {
+  //   if (map.has(x)) {
+  //     return true
+  //   }
+  //   map.set(x, 1)
+  // }
+  // return false
+  const set = new Set()
   for (const x of nums) {
-    if (map.has(x)) {
+    if (set.has(x)) {
       return true
     }
-    map.set(x, 1)
+    set.add(x)
   }
   return false
 }
