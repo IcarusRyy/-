c7971a7c9c4c1a2e5dfcbc943df8cb78f59e4d7f
apollo <1550588466@qq.com>
2024-02-29 00:25:17 +0800

    feat: 144 二叉树的前序遍历  栈实现

--- 算法/树/lc/144二叉树的前序遍历.js
+++ 算法/树/lc/144二叉树的前序遍历.js
@@ -10,20 +10,44 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var preorderTraversal = function (root) {
-  let res = []
 
-  const fun = (node) => {
-    if (node) {
-      // 先根节点
-      res.push(node.val)
-      // 遍历左子树
-      fun(node.left)
-      // 遍历右子树
-      fun(node.right)
+// 递归方式
+// var preorderTraversal = function (root) {
+//   let res = []
+
+//   const fun = (node) => {
+//     if (node) {
+//       // 先根节点
+//       res.push(node.val)
+//       // 遍历左子树
+//       fun(node.left)
+//       // 遍历右子树
+//       fun(node.right)
+//     }
+//   }
+//   fun(root)
+
+//   return res
+// }
+
+// 栈的方式
+var preorderTraversal = function (root) {
+  if (!root) return []
+  let res = []
+  // 根节点入栈
+  let stack = [root]
+  while (stack.length) {
+    const item = stack.pop()
+    res.push(item.val)
+    // 因为是栈结构,所以是先进后出
+    // 所以这里要先把右节点压栈
+    if (item.right) {
+      stack.push(item.right)
+    }
+    // 然后再将左节点压栈
+    if (item.left) {
+      stack.push(item.left)
     }
   }
-  fun(root)
-
   return res
 }
