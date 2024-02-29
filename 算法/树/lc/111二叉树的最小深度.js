fcfb45def324b37aa725f4a2fdb8f5d5972f7dca
apollo <1550588466@qq.com>
2024-02-29 17:39:06 +0800

    feat: 111 二叉树的最小深度 递归

--- 算法/树/lc/111二叉树的最小深度.js
+++ 算法/树/lc/111二叉树的最小深度.js
@@ -31,3 +31,15 @@ var minDepth = function (root) {
     }
   }
 }
+
+// 递归
+var minDepth = function (root) {
+  if (!root) return 0
+  if (!root.left) {
+    return 1 + minDepth(root.right)
+  }
+  if (!root.right) {
+    return 1 + minDepth(root.left)
+  }
+  return 1 + Math.min(minDepth(root.left), minDepth(root.right))
+}
