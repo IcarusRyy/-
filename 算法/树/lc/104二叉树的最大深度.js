60dc0f1366e7623813bf1996b6e3658fc86bad51
apollo <1550588466@qq.com>
2024-02-29 18:04:52 +0800

    feat: 104 二叉树的最大深度 栈 深度优先遍历 DFS

--- 算法/树/lc/104二叉树的最大深度.js
+++ 算法/树/lc/104二叉树的最大深度.js
@@ -8,3 +8,22 @@ var maxDepth = function (root) {
   // 最大深度为左、右子树深度的较大者加1（加上当前节点的深度）
   return Math.max(leftDepth, rightDepth) + 1
 }
+
+// 栈 深度优先遍历 DFS
+var maxDepth = function (root) {
+  if (!root) return 0
+  const stack = [[root, 1]]
+  let max = 1
+
+  while (stack.length) {
+    const [obj, num] = stack.pop()
+
+    // 如果是叶子节点
+    if (!obj.left && !obj.right) {
+      max = Math.max(max, num)
+    }
+    if (obj.left) stack.push([obj.left, num + 1])
+    if (obj.right) stack.push([obj.right, num + 1])
+  }
+  return max
+}
