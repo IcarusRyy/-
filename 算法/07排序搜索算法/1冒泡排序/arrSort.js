c0b656527d5e148bafec6b55c96e43ecf787a1e5
apollo <1550588466@qq.com>
2024-03-01 21:40:44 +0800

    feat: 冒泡排序

---
+++ 算法/07排序搜索算法/1冒泡排序/arrSort.js
@@ -0,0 +1,32 @@
+// 从小到大
+
+function arrSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+let arr = [27, 3, 5, 1, 22, 33, 45, 12, 122, 21]
+console.log(arrSort(arr))
+
+// 从大到小
+
+function arrSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] < arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
