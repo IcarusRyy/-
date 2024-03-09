88d0541f93847eba7ff80b5328c5a03ff1366265
apollo <1550588466@qq.com>
2024-03-09 21:47:18 +0800

    feat: 374猜数字游戏（分治）

--- 算法/07排序搜索算法/7分治/374猜数字大小.js
+++ 算法/07排序搜索算法/7分治/374猜数字大小.js
@@ -1,18 +1,36 @@
 // 二分搜索
+// var guessNum = function (n) {
+//   let low = 1
+//   let high = n
+
+//   while (low <= high) {
+//     const mid = Math.floor((low + high) / 2)
+
+//     const res = guess(mid)
+//     if (res === 0) {
+//       return mid
+//     } else if (res === 1) {
+//       low = mid + 1
+//     } else {
+//       high = mid - 1
+//     }
+//   }
+// }
+
+// 分治
 var guessNum = function (n) {
   let low = 1
   let high = n
-
-  while (low <= high) {
+  const fn = (low, high) => {
     const mid = Math.floor((low + high) / 2)
-
     const res = guess(mid)
     if (res === 0) {
       return mid
     } else if (res === 1) {
-      low = mid + 1
+      return fn(mid + 1, high)
     } else {
-      high = mid - 1
+      return fn(mid - 1, high)
     }
   }
+  return fn(1, n)
 }
