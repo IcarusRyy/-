c5540be5dc115d47e5b7450ee2a556019e45b998
apollo <1550588466@qq.com>
2024-04-01 15:36:23 +0800

    feat: 300最长递增子序列 动态规划

---
+++ 算法/08动态规划/3背包问题/300最长递增子序列.js
@@ -0,0 +1,19 @@
+var lengthOfLIS = function (nums) {
+  // 最长递增子序列
+  // dp[i] 表示以nums[i]结尾的最长递增子序列的长度
+  let dp = new Array(nums.length).fill(1)
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+  }
+  return Math.max(...dp)
+}
+
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+console.log(lengthOfLIS([0, 1, 0, 3, 2, 3]))
+console.log(lengthOfLIS([0, 0]))
+console.log(lengthOfLIS([4, 3, 2, 1]))
