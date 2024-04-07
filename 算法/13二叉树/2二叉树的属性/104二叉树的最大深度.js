a0084891b44f9160e6c762045df9750a07333cd1
apollo <1550588466@qq.com>
2024-04-07 17:26:53 +0800

    feat: 104二叉树的最大深度 利用前序遍历 求解二叉树的最大深度

--- 算法/13二叉树/2二叉树的属性/104二叉树的最大深度.js
+++ 算法/13二叉树/2二叉树的属性/104二叉树的最大深度.js
@@ -12,18 +12,32 @@
  */
 
 // 利用二叉树的最大高度 等于最大深度来求解
+// var maxDepth = function (root) {
+//   if (!root) return 0
+
+//   // 后序遍历
+//   const getHeight = function (node) {
+//     if (!node) return 0
+//     let leftHeight = getHeight(node.left)
+//     let rightHeight = getHeight(node.right)
+
+//     return 1 + Math.max(leftHeight, rightHeight)
+//   }
+//   return getHeight(root)
+// }
+
+// 利用前序遍历来求解二叉树的最大深度
 var maxDepth = function (root) {
-  if (!root) return 0
-
-  // 后序遍历
-  const getHeight = function (node) {
-    if (!node) return 0
-    let leftHeight = getHeight(node.left)
-    let rightHeight = getHeight(node.right)
-
-    return 1 + Math.max(leftHeight, rightHeight)
+  // 记录当前的最深层级
+  let res = 0
+  let getDeep = function (node, cur = 0) {
+    if (!node) {
+      res = Math.max(res, cur)
+      return
+    }
+    getDeep(node.left)
+    getDeep(node.right)
   }
-  return getHeight(root)
+  getDeep(root)
+  return res
 }
-
-// var maxDepth = function (root) {}
