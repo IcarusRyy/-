351d503a4fff47cdd062abc05037641e6b8dc21c
apollo <1550588466@qq.com>
2024-04-08 01:04:56 +0800

    feat: 手写实现treeToArr

---
+++ JS/手写/treeToArr/index.js
@@ -0,0 +1,33 @@
+function treeToArr(tree) {
+  let result = []
+
+  function traverse(node) {
+    if (!node) return
+
+    result.push({ id: node.id, pid: node.pid || null })
+
+    if (node.children && node.children.length > 0) {
+      node.children.forEach(traverse)
+    }
+  }
+  if (Array.isArray(tree)) {
+    tree.forEach(traverse)
+  } else {
+    traverse(tree)
+  }
+
+  return result
+}
+
+let arr = [
+  {
+    id: 1,
+    children: [
+      { id: 2, pid: 1 },
+      { id: 3, pid: 1 },
+    ],
+  },
+  { id: 4, children: [{ id: 5, pid: 2 }] },
+]
+
+console.log(treeToArr(arr))
