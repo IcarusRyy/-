b98d3e7786e509293e690ae5632dc0f43965096a
apollo <1550588466@qq.com>
2024-04-07 14:08:37 +0800

    feat: 手写实现add[1][2][3] 连续调用

---
+++ JS/手写/add[3][2][1]/test3.js
@@ -0,0 +1,17 @@
+function createProxy(val = 0) {
+  return new Proxy(
+    {},
+    {
+      get: function (target, key) {
+        if (key === Symbol.toPrimitive) {
+          return () => val
+        }
+        return createProxy(val + Number(key))
+      },
+    }
+  )
+}
+
+const add = createProxy()
+
+console.log(add[1][2] + 2)
