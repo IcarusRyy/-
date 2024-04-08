ee551e7beb461e440d40661cead4aa373166b89d
apollo <1550588466@qq.com>
2024-04-08 19:34:40 +0800

    feat: 手写实现防抖

---
+++ JS/手写/防抖和节流/防抖/test1.js
@@ -0,0 +1,11 @@
+function myDebounce(fn, wait) {
+  let timer = null
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, wait)
+  }
+}
