32edf89673a2900872c9057d7f2e82a8704e6e37
apollo <1550588466@qq.com>
2024-04-10 17:15:59 +0800

    feat: 清除错误hook

--- JS/手写/hook/useStoreCache.ts
+++
@@ -1,7 +0,0 @@
-import { create, StateCreator } from 'zustand';
-import { persist } from 'zustand/middleware';
-
-export const useStoreCache = (options: StateCreator<any>, attrs: any) => {
-    const storeCache = create(persist(options, attrs) as StateCreator<any>);
-    return storeCache;
-};
