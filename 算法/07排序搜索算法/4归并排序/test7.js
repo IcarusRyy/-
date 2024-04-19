7f97ed2ec64f6ee5222bcaf71853af4747fd424f
apollo <1550588466@qq.com>
2024-04-20 01:47:07 +0800

    feat: 排序算法

---
+++ 算法/07排序搜索算法/4归并排序/test7.js
@@ -0,0 +1,15 @@
+function mergeArr(arr) {
+  if (arr.length < 2) return arr
+  let mid = Math.floor(arr.length / 2)
+  let merge = function (left, right) {
+    let result = []
+    while (left.length && right.length) {
+      result.push(left[0] < right[0] ? left.shift() : right.shift())
+    }
+    return result.concat(left).concat(right)
+  }
+  return merge(mergeArr(arr.slice(0, mid)), mergeArr(arr.slice(mid)))
+}
+let arr = [5, 2, 6, 3, 9, 8]
+
+console.log(mergeArr(arr))
